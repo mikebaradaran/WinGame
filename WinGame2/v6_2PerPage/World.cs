ff756c1c9c2bf16025ec3ca04ec4e13350f26a15
Bob <mike.baradaran@qa.com>
2023-05-14 16:25:08 +0100

    Any world can be selected

---
+++ WinGame2/v6_2PerPage/World.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace WinGame2.TwoPerPape
+{
+    public class World
+    {
+        public Rectangle bounds;
+
+        public Shape[] balls;
+        public World(Rectangle rec)
+        {
+            bounds = rec;
+
+            balls = new Shape[] {
+                new Shape(new Rectangle(bounds.X+10, bounds.Y+10, 30, 30), 1, 3),
+                new Shape(new Rectangle(bounds.X+50, bounds.Y+10, 30, 30), 2, 5),
+                new Shape(new Rectangle(bounds.X+20, bounds.Y+30, 30, 20), 3, 2, ShapeType.Oval),
+                new Shape(new Rectangle(bounds.X+60, bounds.Y+30, 30, 50), 5, 1, ShapeType.Oval),
+                new Shape(new Rectangle(bounds.X+70, bounds.Y+10, 30, 30), 3, 3)
+            };
+        }
+
+        public void moveDraw(Graphics g)
+        {
+            //g.Clear(Color.AntiqueWhite);
+
+            g.FillRectangle(Brushes.Red, bounds);
+
+            foreach (var ball in balls)
+            {
+                ball.move(bounds);
+                if(ball.Type == ShapeType.Oval)
+                    g.FillEllipse(Brushes.Blue, ball.rec);
+                else if (ball.Type == ShapeType.Rectangle)
+                    g.FillRectangle(Brushes.Yellow, ball.rec);
+            }
+
+        }
+    }
+
+}
